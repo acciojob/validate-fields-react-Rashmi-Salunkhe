2fae71b21c58e6c0d1d1c1f5df2e0fe97e8d458d
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-09-26 12:04:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const[state,setState]=useState({name:'',password:''});
+  const [errorMessage, setErrorMessage] = useState("");
+  function handleChange(event){
+    setState({...state,[event.target.name]:event.target.value});
+  }
+
+  function handleClick(){
+    
+    event.preventDefault();
+    if(state.name==='' || state.password===''){
+      setErrorMessage('Both name and password are required');
+    }
+    else{
+      setErrorMessage('');
+    }    console.log(state);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+       <form onSubmit={handleClick}>
+      <input type="text" name="name" value={state.name} onChange={handleChange}/>
+      <input type="password" name="password" value={state.password} onChange={handleChange} />
+      <button>Submit</button>
+      {errorMessage&&(<h1 className="errorMessage">{errorMessage}</h1>)}
+       </form>
+
+
+
     </div>
   )
 }
